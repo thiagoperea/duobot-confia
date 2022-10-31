152e0b7982d82f513f22e93fc131ee8c40dbb16b
Thiago Perea <thiago.perea1@gmail.com>
2022-10-31 17:34:26 -0300

    initial commit

---
+++ app/src/main/java/com/thiagoperea/duobotconfia/presentation/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.thiagoperea.duobotconfia.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.thiagoperea.duobotconfia.R
+import com.thiagoperea.duobotconfia.presentation.api_data.ApiDataActivity
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.apiDataButton).setOnClickListener {
+            val intent = Intent(this, ApiDataActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/thiagoperea/duobotconfia/presentation/api_data/ApiDataActivity.kt
@@ -0,0 +1,13 @@
+package com.thiagoperea.duobotconfia.presentation.api_data
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.thiagoperea.duobotconfia.R
+
+class ApiDataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_api_data)
+    }
+}
