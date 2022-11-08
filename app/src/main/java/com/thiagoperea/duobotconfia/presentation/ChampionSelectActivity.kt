e0c7b41ee93a03372d57e82b4197840950a67e66
Thiago Perea <thiago.perea1@gmail.com>
2022-11-08 17:26:29 -0300

    sound effect on click

--- app/src/main/java/com/thiagoperea/duobotconfia/presentation/ChampionSelectActivity.kt
+++ app/src/main/java/com/thiagoperea/duobotconfia/presentation/ChampionSelectActivity.kt
@@ -1,25 +1,43 @@
 package com.thiagoperea.duobotconfia.presentation
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.thiagoperea.duobotconfia.R
 import com.thiagoperea.duobotconfia.databinding.ActivityChampionSelectBinding
 
 
 class ChampionSelectActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityChampionSelectBinding
+    lateinit var buttonSfxPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChampionSelectBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupSfx()
+        setupButtons()
+    }
+
+    override fun onStop() {
+        buttonSfxPlayer.release()
+        super.onStop()
+    }
+
+    private fun setupSfx() {
+        buttonSfxPlayer = MediaPlayer.create(this, R.raw.sfx_button_click)
+    }
+
+    private fun setupButtons() {
         binding.imageReturn.setOnClickListener {
             finish()
         }
 
         binding.imageRefresh.setOnClickListener {
+            buttonSfxPlayer.start()
             revealChampions()
         }
     }
